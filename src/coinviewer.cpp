36812ce7fcd06379a2d11d542216eb9d620a528f
gabriel <gabriel.mcx@gmail.com>
2014-02-27 11:25:25 +0200

    IMPROVED graphics with antialiasing ( solution found at http://controlsystemiscool.blogspot.ro/2013/06/how-to-enable-anti-aliasing-for-coin3d.html )

--- src/coinviewer.cpp
+++ src/coinviewer.cpp
@@ -1,5 +1,7 @@
 #include <QObject>
 #include <QWidget>
+#include <QtGui/QApplication>
+#include <QtOpenGL/QGLWidget>
 #include <Inventor/Qt/SoQt.h>
 #include <Inventor/Qt/viewers/SoQtExaminerViewer.h>
 #include <Inventor/nodes/SoSeparator.h>
@@ -15,6 +17,19 @@ coinviewer::coinviewer(QWidget *parent , SoSeparator* scene , SoCamera *first_pe
 
   SoQt::init(this);
   SoQtExaminerViewer *viewer = new SoQtExaminerViewer(this);
+  
+  /* antialiasing (three passes improves the graphics) */
+  
+  QGLWidget* qglw = (QGLWidget*) viewer->getGLWidget();
+  QGLFormat fmt = qglw->format();
+
+  fmt.setSampleBuffers(true);
+  fmt.setSamples(3);
+  qglw->setFormat(fmt);
+  qglw->makeCurrent();
+  glEnable(GL_MULTISAMPLE);
+
+  /* end */
   
   viewer->setSceneGraph(scene);
   viewer->setSize(SbVec2s(1024,768));
