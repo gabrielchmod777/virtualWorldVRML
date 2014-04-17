ed4602a23fab94bbc6cfce836346c5c98b617a17
gabriel <gabriel.mcx@gmail.com>
2014-04-17 20:35:33 +0300

    ..

--- rmextra.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm -r .deps/ L3* Makefile Makefile.in aclocal.m4 autom4te.cache/ config.h* config.log  config.status configure depcomp install-sh missing moc.mk~ moc_* stamp-h1 src/stamp-h1 src/config.h.in src/*~ 
