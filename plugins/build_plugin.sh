542d64e4d308a46737308b774192be410aa5ccbf
Gabriel Muresan <gabriel.mcx@gmail.com>
2014-07-05 01:54:36 +0300

    ALMOST OK PLUG-ins

--- plugins/build_plugin.sh
+++ plugins/build_plugin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Coin3D and SoQt related
+# Coin3D , SoQt , Boost
 #
 CPPFLAGS_COIN=`soqt-config --cppflags`
 CXXFLAGS_COIN=`soqt-config --cxxflags`
@@ -14,13 +14,21 @@ LIBS_QT=`soqt-config --libs`
 INCLUDEDIR_QT=" -I/usr/include/qt4 -I/usr/include/qt4/QtCore -I/usr/include/qt4/QtGui -I/usr/include/qt4/QtOpenGL  -I/usr/include/qt4/QtWebKit "
 #`soqt-config --includedir`
 
+BOOST_CPPFLAGS=" -pthread " 
+BOOST_FILESYSTEM_LDFLAGS=" -L/usr/local/lib -Wl,-R,/usr/local/lib "
+BOOST_FILESYSTEM_LIBS=" -lboost_filesystem "
+BOOST_SYSTEM_LDFLAGS=" -L/usr/local/lib -Wl,-R,/usr/local/lib "
+BOOST_SYSTEM_LIBS=" -lboost_system "
+BOOST_THREAD_LDFLAGS=" -L/usr/local/lib -Wl,-R,/usr/local/lib "
+BOOST_THREAD_LIBS=" -lboost_thread -lboost_system -pthread "
+
 #
 # [ -Wall ] display all warnings
 # [ -fPIC ] generate Position Independent Code
 #
 #  MODEL g++ -c -Wall -fPIC plugin1.cpp  -o plugin1.o
 
-g++ -c -Wall -fPIC $INCLUDEDIR_QT $1 -o $2.o 
+g++ -std=c++11 -c -Wall -fPIC $INCLUDEDIR_QT $1 -o $2.o 
 
 #
 # [ -Wl,-snoname,<name> ] instuct the linker the
@@ -29,7 +37,7 @@ g++ -c -Wall -fPIC $INCLUDEDIR_QT $1 -o $2.o
 #
 #g++             -shared -Wl,-soname,cos.so -o cos.so plugin1.o
 
-g++ $CPPFLAGS_COIN $CXXFLAGS_COIN $CPPFLAGS_QT $CXXFLAGS_QT -I$INCLUDEDIR_QT -shared -Wl,-soname,$3.so -o $3.so $2.o $LDFLAGS_COIN $LIBS_COIN  $LDFLAGS_QT $LIBS_QT
+g++ -std=c++11 $BOOST_CPPFLAGS $CPPFLAGS_COIN $CXXFLAGS_COIN $CPPFLAGS_QT $CXXFLAGS_QT -I$INCLUDEDIR_QT -shared -Wl,-soname,$3.so -o $3.so $2.o  $BOOST_SYSTEM_LDFLAGS $BOOST_SYSTEM_LIBS $BOOST_THREAD_LIBS $BOOST_THREAD_LDFLAGS $BOOST_FILESYSTEM_LDFLAGS $BOOST_FILESYSTEM_LIBS $LDFLAGS_COIN $LIBS_COIN  $LDFLAGS_QT $LIBS_QT
 # create links
 #ln -sf $3.so.1.0 $3.so.1
 #ln -sf $3.so.1.0 $3.so
--- plugins/plugin_avatar.cpp
+++ plugins/plugin_avatar.cpp
@@ -1,33 +1,110 @@
 #include "../src/plugin.h"
 #include "../src/vrml_utils.h"
 #include <Inventor/nodes/SoSeparator.h>
+#include <Inventor/nodes/SoTranslation.h>
+#include <Inventor/nodes/SoRotation.h>
 #include <Inventor/nodes/SoCone.h>
 #include <string>
 #include <iostream>
-
+#include <cstdlib>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <sstream>
 
 #include "../src/vrml_utils.cpp"
 
 extern "C" void get_plug_in( std::string data, SoSeparator* insertionPoint )
 {
 
-  SoSeparator* _3d_obj = (SoSeparator*)findNode("other",insertionPoint);
+  using boost::property_tree::ptree;
+  ptree pt;
 
+  std::stringstream streammed_msg;
+  streammed_msg<<data;
 
+  read_xml(streammed_msg, pt);
 
-  std::cout<<std::endl<<"      PLUG IN      ";
+  std::string action = pt.get<std::string>("response.action");
+  std::string gender = pt.get<std::string>("response.avatar_type_id");
+  std::string name = pt.get<std::string>("response.other_user");
+ 
+  std::string rotN = "MOC_ROTATE_"+name;
+  std::string movN = "MOC_MOVE_"+name;
+ 
+  //create grapichs first time only
+  SoSeparator* _3d_obj = (SoSeparator*)findNode(name.c_str(),insertionPoint);
+
   std::cout<<std::endl<<data;
   if( _3d_obj == NULL )
     {
-      std::cout<<std::endl<<"CREATE A CONE";
+      std::cout<<std::endl<<"CREATE A MOC AVATAR";
+
+      SoTranslation *translation = new SoTranslation;
+      translation->translation.setValue(0.1,0,0);
+      translation->setName( movN.c_str() );
+      SoRotation *rotation = new SoRotation;
+      rotation->rotation.setValue(SbVec3f(0, 1, 0), 0.1);
+      rotation->setName( rotN.c_str() );
+
       _3d_obj = new SoSeparator;
-      _3d_obj->setName("other");
-      _3d_obj->addChild( new SoCone );
+      _3d_obj->setName(name.c_str());
+      _3d_obj->addChild( translation );
+      _3d_obj->addChild( rotation );
+
+      if ( gender == "0" )
+	{
+	  _3d_obj->addChild(get_scene_graph_from_file("/usr/local/share/l3dclient/avatar_male.wrl"));
+	}
+      else
+	{
+	  _3d_obj->addChild(get_scene_graph_from_file("/usr/local/share/l3dclient/avatar_female.wrl"));
+	}
+
       insertionPoint->addChild( _3d_obj );
     }
-  else 
+  //end create graphics
+
+  //execute code for the current action
+  if ( action == "rotate" )
     {
-      std::cout<<std::endl<<"CONE EXISTS";
+      std::string s_angle = pt.get<std::string>("response.degrees");
+      float angle = std::stof( s_angle.c_str() );
+      SoRotation* rotation = (SoRotation*) findNode( rotN.c_str(), insertionPoint );
+      if ( rotation == NULL )
+	{
+	  rotation = new SoRotation;
+	  rotation->setName( rotN.c_str() );
+	  _3d_obj->addChild( rotation );
+	}
+
+      rotation->rotation.setValue(SbVec3f(0, 1, 0), angle);
     }
-  
+  else if ( action == "move" )
+    {
+      float x,z;
+      
+      std::string s_X = pt.get<std::string>("response.x");
+      x = std::stof( s_X.c_str() );
+      std::string s_Z = pt.get<std::string>("response.z");
+      z = std::stof( s_Z.c_str() );
+      
+      //SbVec3f pos(x,y,z);
+      SoTranslation* translation = (SoTranslation*) findNode( movN.c_str(), insertionPoint );
+      if ( translation == NULL )
+	{
+
+	  translation = new SoTranslation;
+	  translation->setName( movN.c_str() );
+	  _3d_obj->addChild( translation );
+      
+	}
+      translation->translation.setValue(x,0.0,z);
+    
+    }
+  else if ( action == "talk" )
+    {
+
+    }
+
+
 }
